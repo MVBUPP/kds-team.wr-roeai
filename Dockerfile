c51a958845fd1bae5e0906febcc96d4144fd51d1
Maggie Bupp <mvb6309@psu.edu>
2024-07-30 12:32:44 -0400

    adding api

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,8 @@ FROM quay.io/keboola/docker-custom-python:latest
 
 COPY . /code/
 WORKDIR /data/
+
+RUN pip install -r /code/requirements.txt
+RUN python -m pip install requests
+
 CMD ["python", "-u", "/code/main.py"]
--- main.py
+++ main.py
@@ -1,2 +1,32 @@
 
 print("testing deploy")
+import requests
+from keboola.component.base import ComponentBase
+from keboola.component.exceptions import UserException
+# Load the Component library to process the config file
+from keboola.component import CommonInterface
+
+class Component(ComponentBase):
+    def __init__(self):
+        super().__init__()
+    def run(self):       
+        params = configuration.parameters
+        TOKEN = params['BearerToken']
+
+        url = "https://api.roe-ai.com/v1/datasets/files/upload/"
+
+        payload = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"file\"\r\n\r\n<string>\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"dataset_id\"\r\n\r\n<string>\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"metadata\"\r\n\r\n<any>\r\n-----011000010111000001101001--\r\n\r\n"
+        headers = {
+            "Authorization": "Bearer {TOKEN}",
+            "Content-Type": "multipart/form-data"
+        }
+
+        response = requests.request("POST", url, data=payload, headers=headers)
+
+        print(response.text)
+
+if __name__ == "__main__":
+        comp = Component()
+        # this triggers the run method by default and is controlled by the configuration.action parameter
+        comp.execute_action()
+    
