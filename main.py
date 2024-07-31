d42961f6309dbf840449bf5a2aeb6a9be0f51dce
Maggie Bupp <mvb6309@psu.edu>
2024-07-31 12:59:15 -0400

    API call not working

--- main.py
+++ main.py
@@ -2,9 +2,6 @@
 print("testing deploy")
 import requests
 from keboola.component.base import ComponentBase
-from keboola.component.exceptions import UserException
-# Load the Component library to process the config file
-from keboola.component import CommonInterface
 
 class Component(ComponentBase):
     def __init__(self):
@@ -12,18 +9,20 @@ class Component(ComponentBase):
     def run(self):       
         params = self.configuration.parameters
         Token = params['BearerToken']
-
+        file=params['FileName']
+        dataset=params['Dataset']
+        metadata=params['Metadata']
+        
         url = "https://api.roe-ai.com/v1/datasets/files/upload/"
+        with open(file, "rb") as File:
+            payload = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"file\"\r\n\r\n{FileName}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"dataset_id\"\r\n\r\n{dataset}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"metadata\"\r\n\r\n{metadata}\r\n-----011000010111000001101001--\r\n\r\n".format(FileName=File, metadata=metadata, dataset=dataset)
+            headers = {
+                "Authorization": "Bearer {Token}".format(Token=Token),
+                "Content-Type": "multipart/form-data"
+            }
+            response = requests.request("POST", url, data=payload, headers=headers)
 
-        payload = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"file\"\r\n\r\n<string>\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"dataset_id\"\r\n\r\n<string>\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"metadata\"\r\n\r\n<any>\r\n-----011000010111000001101001--\r\n\r\n"
-        headers = {
-            "Authorization": "Bearer {Token}".format(Token=Token),
-            "Content-Type": "multipart/form-data"
-        }
-
-        response = requests.request("POST", url, data=payload, headers=headers)
-
-        print(response.text)
+            print(response.text)
 
 if __name__ == "__main__":
         comp = Component()
