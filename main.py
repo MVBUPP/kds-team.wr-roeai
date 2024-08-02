8065e2394d8b906bc04693ab03a1f283dd9ddf31
Maggie Bupp <mvb6309@psu.edu>
2024-08-02 12:19:11 -0400

    working

--- main.py
+++ main.py
@@ -2,35 +2,43 @@
 print("testing deploy")
 import requests
 from keboola.component.base import ComponentBase
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 class Component(ComponentBase):
     def __init__(self):
         super().__init__()
+        
     def run(self):       
         params = self.configuration.parameters
         Token = params['BearerToken']
         file=params['FileName']
         dataset=params['Dataset']
         metadata=params['Metadata']
-        
+        content_type=params["Content_Type"]
         url = "https://api.roe-ai.com/v1/datasets/files/upload/"
          
-        
-        # Make the POST request
-        with open(file, "rb") as File:
-                
-            payload = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"file\"\r\n\r\n{FileName}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"dataset_id\"\r\n\r\n{dataset}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"metadata\"\r\n\r\n{metadata}\r\n-----011000010111000001101001--\r\n\r\n".format(FileName=File, metadata=metadata, dataset=dataset)
-            headers = {
-                "Authorization": "Bearer {Token}".format(Token=Token),
-                "Content-Type": "multipart/form-data"
-            }
-            response = requests.request("POST", url, data=payload, headers=headers)
-            
-            print(response.status_code)
-            print(response.text)
-            print("Request Headers:", response.request.headers)
-            print("Request Body:", response.request.body)
+        # dataset_id and metadata are OPTIONAL, so omit the payload if you don't need them
+        payload = {
+            'dataset_id': dataset,
+            'metadata': metadata
+        }
 
+        # REQUIRED
+        files=[
+        ('file', (file, open(file,'rb'), content_type))
+        ]
+
+        headers = {
+        "Authorization": "Bearer {Token}".format(Token=Token)
+        }
+
+        if(dataset or metadata):
+             response = requests.request("POST", url, headers=headers, data=payload, files=files)
+        else:
+            response = requests.request("POST", url, headers=headers, files=files)
+
+        print(response.text)
+        
 if __name__ == "__main__":
         comp = Component()
         # this triggers the run method by default and is controlled by the configuration.action parameter
