4d74d8f535473e61d1a4c39114e0de13123228a1
Maggie Bupp <mvb6309@psu.edu>
2024-08-21 15:11:23 -0400

    encryption fixed and some logging

--- main.py
+++ main.py
@@ -1,40 +1,54 @@
 
 print("testing deploy")
+import logging
 import requests
 import csv
 import os
+from kbcstorage.client import Client
 from keboola.component import CommonInterface
 from keboola.component.base import ComponentBase
 from requests_toolbelt.multipart.encoder import MultipartEncoder
+from configuration import Configuration
+# from client.googleai_client import GoogleAIClient
+
 
 class Component(ComponentBase):
     def __init__(self):
         super().__init__()
 
+    def _init_configuration(self) -> None:
+        self.validate_configuration_parameters(Configuration.get_dataclass_required_parameters())
+        self._configuration: Configuration = Configuration.load_from_dict(self.configuration.parameters)
+
     def run(self):       
         params = self.configuration.parameters
-        Token = params['BearerToken']
-        TABLENAME=params["Dataset"]
+        self._init_configuration()
+        Token = self._configuration.credentials.pswd_api_token
+     #Token = self.configuration.image_parameters.get(KEY_API_TOKEN)
+     #    credentials=params["credentials"]
+     #    Token= credentials["api_token"]
+     
+        TABLENAME=params["dataset"]
         url = "https://api.roe-ai.com/v1/database/query/"
         headers = {
         "Authorization": "Bearer {Token}".format(Token=Token)
         }
      #    UNCOMMENT
-        ci=CommonInterface()
-        tables=ci.configuration.tables_input_mapping
+     #    ci=CommonInterface()
+     #    tables=ci.configuration.tables_input_mapping
         ###TEMP FILE ACCESS###
-     #    path = "in/tables/"
-     #    dir_list = os.listdir(path)
-     #    fileName=dir_list[0]
+        path = "in/tables/"
+        dir_list = os.listdir(path)
+        fileName=dir_list[0]
 
-        for table in tables:
-          # inName=table.destination
-          table_def=ci.get_input_table_definition_by_name(table.destination)
+     #    for table in tables:
+     #      # inName=table.destination
+     #      table_def=ci.get_input_table_definition_by_name(table.destination)
      #UNCOMMENT TO HERE
         # open csv file to parse column str for query
      #    with open('in/tables/{filename}'.format(filename=fileName), mode ='r')as file:
-          with open(table_def.full_path, mode ='r')as file: #UNCOMMENT 
-     #    with open('in/tables/{filename}'.format(filename=fileName), mode ='r')as file:
+          # with open(table_def.full_path, mode ='r')as file: #UNCOMMENT 
+        with open('in/tables/{filename}'.format(filename=fileName), mode ='r')as file:
                csvFile = csv.reader(file)
                line1 = next(csvFile)
                for i in range(len(line1)):
@@ -48,20 +62,28 @@ class Component(ComponentBase):
                #create table
                payload = {"query": "CREATE TABLE {tableName} {columns} ENGINE = Memory AS SELECT 1;".format(tableName=TABLENAME, columns=colstr)}
                response = requests.request("POST", url, json=payload, headers=headers)
+               if (response.status_code == 200):
+                     logging.info("Request to create table in RoeAI succeeded.")
+               elif (response.status_code == 404):
+                     logging.info("Request to create table in RoeAI failed.")
                
                # remove extra column from column str
                payload={"query": "ALTER TABLE {tableName} DROP COLUMN buffercolumn;".format(tableName=TABLENAME)}
                response = requests.request("POST", url, json=payload, headers=headers)
-          
-               
+               if (response.status_code == 200):
+                     logging.info("Request to drop column in RoeAI succeeded.")
+               elif (response.status_code == 404):
+                     logging.info("Request to drop column in RoeAI failed.") 
+
                 # Insert rest of data
                tableList=""
                # i=0
                add=True
+               logging.info("Table is being loaded into RoeAI...")
                for lines in csvFile:
                          for elements in lines:
                                if elements.__contains__("'"):
-                                   #   
+                                   logging.info("Entry contains unescaped ' in column.")   
                                    add=False
                          if add:
                               tableList+="{newLine}, ".format(newLine=tuple(lines))
@@ -78,7 +100,10 @@ class Component(ComponentBase):
                tableList=tableList[:-2]
                payload = {"query": "INSERT INTO {tableName} VALUES {line}".format(tableName=TABLENAME,line=tableList)}
                response = requests.request("POST", url, json=payload, headers=headers)
-               print(response.text)
+               if (response.status_code == 200):
+                     logging.info("Request to insert table into RoeAI succeeded.")
+               elif (response.status_code == 404):
+                     logging.info("Request to insert table into RoeAI failed.")
 
         ######Potential Single File implementation######
         # dataset_id and metadata are OPTIONAL, so omit the payload if you don't need them
